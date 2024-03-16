0e6ed6a2d40fde7a58d90bd2474dfd71a50f5793
rizkyalvie <rizkyalviee@gmail.com>
2024-03-16 21:52:13 +0700

    Hello World

--- index.mjs
+++ index.mjs
@@ -1,24 +1,27 @@
 import express from 'express'
 import http from 'http'
-import {Server} from 'socket.io'
+// import {Server} from 'socket.io'
 import cors from 'cors'
 import router from './src/routes/index.js'
 import dotenv from "dotenv";
 dotenv.config()
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server, {
-    cors:{
-        origin: '*'
-    }
-})
-import socketIo from './src/socket/index.js'
-socketIo(io)
+// const io = new Server(server, {
+//     cors:{
+//         origin: '*'
+//     }
+// })
+// import socketIo from './src/socket/index.js'
+// socketIo(io)
 
 const port = 5000
 app.use(express.json())
 app.use(cors())
 
+app.use('/', (req, res) => {
+    res.send('Hello world')
+})
 app.use('/api/v1/', router)
 app.use('/uploads', express.static('uploads'))
 
