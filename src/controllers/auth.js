6808c32ee75766654eebf489101d69d860899927
rizkyalvie <rizkyalviee@gmail.com>
2024-03-16 21:31:19 +0700

    DB

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,9 @@
 // import model
 const { users, profile } = require("../../models");
+const { PrismaClient } = require("@prisma/client");
+
+// import prisma client
+const prisma = new PrismaClient();
 
 // import joi validation
 const Joi = require("joi");
@@ -33,36 +37,37 @@ exports.register = async (req, res) => {
     // we hash password from request with salt
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    const newUser = await users.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
-      status: 'customer'
+    const newUser = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+        status: "customer",
+      },
     });
 
-    await profile.create({
-      image: 'blank-profile.png',
-      phone: '',
-      gender: '',
-      address: '',
-      idUser: newUser.id
-    });
-
+    // await profile.create({
+    //   image: 'blank-profile.png',
+    //   phone: '',
+    //   gender: '',
+    //   address: '',
+    //   idUser: newUser.id
+    // });
 
     // generate token
     const token = jwt.sign({ id: users.id }, process.env.TOKEN_KEY);
-    
-    res.status(200).send({
+
+    res.status(200).json({
       status: "success...",
       data: {
         name: newUser.name,
         email: newUser.email,
-        token
+        token,
       },
     });
   } catch (error) {
     console.log(error);
-    res.status(500).send({
+    res.status(500).json({
       status: "failed",
       message: "Server Error",
     });
@@ -150,13 +155,14 @@ exports.checkAuth = async (req, res) => {
 
     res.send({
       status: "success...",
-        user: {
-          id: dataUser.id,
-          name: dataUser.name,
-          email: dataUser.email,
-          status: dataUser.status,
+      user: {
+        id: dataUser.id,
+        name: dataUser.name,
+        email: dataUser.email,
+        status: dataUser.status,
       },
-    });console.log(data)
+    });
+    console.log(data);
   } catch (error) {
     console.log(error);
     res.status({
